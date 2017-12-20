0e13d4a5ec4a27a3214aa9b318f30b9c22958ebf
= <sammygill98@gmail.com>
2017-12-20 14:27:36 -0800

    connected to mongo database

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
+var mongoClient = require("mongodb").MongoClient;
+var db = undefined;
 var app = express();
 var dir = __dirname;
 
-app.listen(3000, function() {
-  console.log("listening on 3000");
+mongoClient.connect("mongodb://ersp:abc123@ds044917.mlab.com:44917/smart-lock", (err, database) => {
+  if(err) {
+    return console.log(err);
+  }
+
+  db = database;
+  app.listen(3000, function() {
+    console.log("listening on 3000");
+  })
 })
 
 app.get("/", (req, res) => {
