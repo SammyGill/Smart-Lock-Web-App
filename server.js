ff1dd46493cebb42d2392a6c0b516313e65c506a
= <sammygill98@gmail.com>
2017-12-21 02:15:35 -0800

    users successfully entered into database when first logging into website. after an existing user logs in, they are redirected to the dashboard

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ var mongoClient = require("mongodb").MongoClient;
 var db = undefined;
 var app = express();
 var dir = __dirname;
+var bodyParser = require('body-parser');
 
 // Used to make the server look in our directory for 
   // our javascript, css, and other files
 app.use(express.static(dir));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 mongoClient.connect("mongodb://ersp:abc123@ds044917.mlab.com:44917/smart-lock", (err, database) => {
   if(err) {
     return console.log(err);
   }
 
-  db = database;
+  db = database.db("smart-lock");
   app.listen(3000, function() {
     console.log("listening on 3000");
   })
@@ -21,4 +24,22 @@ mongoClient.connect("mongodb://ersp:abc123@ds044917.mlab.com:44917/smart-lock", 
 
 app.get("/", (req, res) => {
   res.sendFile(dir + "/views/index.html");
+})
+
+app.get("/authenticate", (req, res) => {
+  var email = req.query.email;
+  db.collection("users").find({user: email}).toArray((err, result) => {
+    if(result.length) {
+      res.send({redirect: "/dashboard"});
+    }
+    else {
+      db.collection("users").insert({user: email}, (err, doc) => {
+      })
+    }
+  })
+})
+
+app.get("/dashboard", (req, res) => {
+  console.log("here");
+  res.sendFile(dir + "/views/dashboard.html");
 })
