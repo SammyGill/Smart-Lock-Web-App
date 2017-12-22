5695a4f203f3caae7b366a4badb1bd7cf7e3476f
= <sammygill98@gmail.com>
2017-12-22 13:07:28 -0800

    added cookies and sessions

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ var db = undefined;
 var app = express();
 var dir = __dirname;
 var bodyParser = require('body-parser');
+var session = require("client-sessions");
 
 // Used to make the server look in our directory for 
   // our javascript, css, and other files
 app.use(express.static(dir));
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  cookieName: 'smartlocksession',
+  secret: 'random_string_goes_here',
+  duration: 30 * 60 * 1000,
+}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+
 mongoClient.connect("mongodb://ersp:abc123@ds044917.mlab.com:44917/smart-lock", (err, database) => {
   if(err) {
     return console.log(err);
@@ -28,12 +34,16 @@ app.get("/", (req, res) => {
 
 app.get("/authenticate", (req, res) => {
   var email = req.query.email;
+  var req = req;
   db.collection("users").find({user: email}).toArray((err, result) => {
     if(result.length) {
+      req.smartlocksession.user = email;
       res.send({redirect: "/register"});
     }
     else {
-      db.collection("users").insert({user: email}, (err, doc) => {
+      db.collection("users").insert({user: email, lockId: null, role: null}, (err, doc) => {
+      req.smartlocksession.user = email;
+      res.send({redirect: "/register"});
       })
     }
   })
