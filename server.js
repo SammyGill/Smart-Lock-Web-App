875bf31ce9db25a1fec665219eedf86c03b6d2b9
Sammy Gill <sammygill98@gmail.com>
2018-02-08 18:35:30 -0800

    conflict'

--- server.js
+++ server.js
@@ -11,7 +11,62 @@ var d = new Date();
 var module = require("../Module/index.js");
 //var google = require('googleapis');
 
+function isLoggedIn(user) {
+   return((user != undefined));
+}
 
+
+function getTime() {
+   var d = new Date();
+   var minutes = d.getMinutes();
+   var hours = d.getHours();
+   if (d.getMinutes() < 10) {
+      minutes = "0" + minutes;
+   }
+   if (d.getHours() > 12) {
+      hours = hours % 12;
+   }
+   var date = hours + ":" + minutes
+      if (d.getHours()/12 == 0) {
+         date = date + " AM";
+      } else {
+         date = date + " PM";
+      }
+   return date;
+}
+
+function convertToMilitary(time) {
+   if(time.indexOf("PM") != -1) {
+      time = time.replace("PM", "");
+      time = time.replace(" ", "");
+      var timeArray = time.split(":");
+      timeArray[0] = parseInt(timeArray[0]);
+      if(timeArray[0] != 12) {
+         timeArray[0] += 12;
+      }
+
+      var timeString = parseInt(timeArray[0].toString() + timeArray[1]);
+      return timeString;
+   }
+   time = time.replace("AM", "");
+   time = time.replace(" ", "");
+   var timeArray = time.split(":");
+   return (timeArray[0] + timeArray[1]);
+}
+
+function checkRestrictions(inputArray, dbArray) {
+   var inputStart = inputArray[0];
+   var inputEnd = inputArray[1];
+   for(var i = 0; i < dbArray.length; dbArray++) {
+      if(inputStart < dbArray[i][1] && inputStart > dbArray[i][0]) {
+         return false;
+      }
+      if(inputEnd < dbArray[i][1] && inputEnd > dbArray[i][0]) {
+         return false;
+      }
+   }
+   return true;
+}
 
 // Used to make the server look in our directory for
 // our javascript, css, and other files
