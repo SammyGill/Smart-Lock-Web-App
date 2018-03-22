a4b77039f3aed44bb2d08f1899a7d796812086da
Sammy Gill <sammygill98@gmail.com>
2018-03-22 12:46:54 -0700

    added comments

--- index.js
+++ index.js
@@ -91,23 +91,67 @@ function withinBounds(username, lockId, state) {
     }
     return true;
   })
+
+  /**
+   * Determines where a user can perform the lock action at the particular time.
+   * A user can lock if:
+   *  1.) They are an owner of the lock
+   *  2.) They are an admin of the lock
+   *  3.) They are a member of the lock and do not conflict with any restrictions
+   * 
+   * @param {string} username - username of the user we are querying for
+   * @param {int} lockId - ID of the lock in question
+   */
 function canLock(username, lockId) {
   return (isOwner(username, lockId) || isAdmin(username, lockId) || withinBounds(username, lockId, "lock"));
 }
 
+  /**
+   * Determines where a user can perform the unlock action at the particular time.
+   * A user can lock if:
+   *  1.) They are an owner of the lock
+   *  2.) They are an admin of the lock
+   *  3.) They are a member of the lock and do not conflict with any restrictions
+   * 
+   * @param {string} username - username of the user we are querying for
+   * @param {int} lockId - ID of the lock in question
+   */
 function canUnlock(username, lockId) {
   return (isOwner(username, lockId) || isAdmin(username, lockId) || withinBounds(username, lockId, "unlock"));
 }
 
+  /**
+   * Determines whether a user is allowed to create a lock event. A user
+   * can create a lock event at a specified time if they are able to
+   * lock at that time.
+   * 
+   * @param {string} username - username of the user we are querying for
+   * @param {int} lockId - ID of the lock we are checking 
+   */
 function canCreateLockEvent(username, lockId) {
   return canLock(username, lockId);
 }
 
+/**
+ * Determines whether a user is allowed to create an ulock event or not.
+ * A User can create an unlock event at a specified time if they are able
+ * to unlock at that time.
+ * 
+ * @param {string} username - user of the user we are querying for
+ * @param {int} lockId ID of the lock we are checking
+ */
 function canCreateUnlockEvent(username, lockId) {
   return canUnlock(username, lockId);
 }
 
- exports.getLockInfo = function(lockId, username, callback) {
+/**
+ * Given a lockId, returns the name of the lock
+ * 
+ * @param {int} lockId - ID of the lock we are getting information for
+ * @param {string} username - I THINK WE CAN DELETE THIS?
+ * @param {function} callback - callback function takine gusername and lock name
+ */
+exports.getLockInfo = function(lockId, username, callback) {
   let lockName = undefined;
   db.collection("locks").find({lockId: lockId}).toArray((err, result) => {
     lockName = result[0].lockName;
@@ -116,20 +160,19 @@ function canCreateUnlockEvent(username, lockId) {
   })
 }
 
+
+/**
+ * Given a lockId, returns the status - lock or unlocked
+ * 
+ * @param {int} lockId - ID of the lock we are checking
+ * @param {function} callback - callback that takes the status of the lock
+ */
 exports.getLockStatus = function(lockId, callback) {
   let lockName = undefined;
   db.collection("locks").find({lockId: lockId}).toArray((err, result) => {
     console.log(result[0].status);
     status = result[0].status;
     callback({status: status});
-  })
-}
-
-exports.canLock = function(lockId, username, callback) {
-  db.collection("locks").find({lockId: lockId}).toArray((err, result) => {
-    if (result[0] != null && result[0].owner == username) {
-      callback({canLock: true});
-    }
   })
 }
 
@@ -144,58 +187,32 @@ exports.connectServer = function() {
   })
 }
 
-exports.findUser = function(user) {
-  db.collection("users").find({username: user}).toArray((err, result) => {
-    if(result[0]) {
-      console.log("found a user!!!");
-    }
-    else {
-      console.log("did not find a user!");
-    }
-  })
-}
-
 exports.isLoggedIn = function(user) {
   return((user != undefined));
 },
 
 
-function getTime() {
-  let d = new Date();
-  let minutes = d.getMinutes();
-  let hours = d.getHours();
-  if (d.getMinutes() < 10) {
-   minutes = "0" + minutes;
- }
- if (d.getHours() > 12) {
-   hours = hours % 12;
- }
- let date = hours + ":" + minutes
- if (d.getHours() < 12) {
-  date = date + " AM";
-} else {
-  date = date + " PM";
-}
-return date;
-}
-
+/**
+ * Returns current time
+ */
 exports.getTime = function() {
   let d = new Date();
   let minutes = d.getMinutes();
   let hours = d.getHours();
   if (d.getMinutes() < 10) {
-   minutes = "0" + minutes;
- }
- if (d.getHours() > 12) {
-   hours = hours % 12;
- }
- let date = hours + ":" + minutes
- if (d.getHours() < 12) {
-  date = date + " AM";
-} else {
-  date = date + " PM";
-}
-return date;
+    minutes = "0" + minutes;
+  }
+  if (d.getHours() > 12) {
+    hours = hours % 12;
+  }
+  let date = hours + ":" + minutes
+  if (d.getHours() < 12) {
+    date = date + " AM";
+  } 
+  else {
+    date = date + " PM";
+  }
+  return date;
 };
 
 exports.convertToMilitary = function(time) {
@@ -210,11 +227,11 @@ exports.convertToMilitary = function(time) {
 
   let timeString = parseInt(timeArray[0].toString() + timeArray[1]);
   return timeString;
-}
-time = time.replace("AM", "");
-time = time.replace(" ", "");
-let timeArray = time.split(":");
-return (parseInt(timeArray[0] + timeArray[1]));
+  }
+  time = time.replace("AM", "");
+  time = time.replace(" ", "");
+  let timeArray = time.split(":");
+  return (parseInt(timeArray[0] + timeArray[1]));
 }
 
 exports.checkRestrictions = function(inputArray, dbArray) {
