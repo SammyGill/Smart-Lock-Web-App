dc2ae2006382535af600772b71687d976e3bfc1b
= <sammygill98@gmail.com>
2018-03-27 18:18:58 -0700

    added users and restrictions to edit users page

--- js/scripts.js
+++ js/scripts.js
@@ -121,10 +121,12 @@ function addMember() {
 }
 
 function getMembers() {
+  console.log("hello");
   $.get("/getMembers", function(data) {
     var list = document.getElementById("membersList");
     for(var i = 0; i < data.members.length; i++) {
-      var member = document.createElement("li");
+      console.log(data.members[i]);
+      var member = document.createElement("option");
       member.appendChild(document.createTextNode(data.members[i] /*+ ": " + data.fullnames[i]*/));
       list.appendChild(member);
     }
