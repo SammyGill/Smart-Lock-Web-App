ac1ab503a477df8e3826d88a96fda65d335bc321
zhou yuwei <zhouyuwei@Yuweis-MacBook-Pro.local>
2018-04-08 19:30:01 -0700

    finished revoke admin

--- index.js
+++ index.js
@@ -152,8 +152,8 @@
       return false;
     }
     return (role == 0);
- 
-}
+
+  }
 
 
 /**
@@ -167,13 +167,14 @@
  */
  function isAdmin(userObject, lockId) {
   let role = searchLocks(lockId, userObject.locks);
+  //console.log("in isAdmin: " + userObject.username + role);
     // returns false here if person isn't in lock
     if(role == undefined) {
       return false;
     }
     return (role == 1);
-  
-}
+
+  }
 
 
 
@@ -618,19 +619,35 @@ return true;
  * @param: lockId - the lock id of lock whose admins we want 
  * @return: array of admins
  */
-exports.getLockAdmins = function(lockId, callback) {
-  db.collection("locks").find({lockId: lockId}).toArray((err, result) => {
+ exports.getLockAdmins = function(lockId, callback) {
+
+  db.collection("locks").find({lockId: lockId}).toArray((err, result) => {  
     let allMembers = result[0].members;
-    console.log(allMembers);
     let adminArray = [];
-    for (let i = 0; i < allMembers.length; i++) {
-      getUser(allMembers[i], function(user) {
-        if (isAdmin(user, lockId)) {
-          adminArray.push(allMembers[i]);
+
+    async.each(allMembers, function(member, callback) {
+      db.collection("users").find({"username": member}).toArray((err, result) => {
+        for(let i = 0;  i < result[0].locks.length; i++) {
+          if(result[0].locks[i].lockId == lockId && result[0].locks[i].role == 1 ) {
+            adminArray.push(member);
+            console.log("inside if: " + adminArray);
+          }
         }
+        callback();
       })
+    }, function(err) {
+    // if any of the file processing produced an error, err would equal that error
+    if( err ) {
+      console.log("There is some error!");
+    } else {
+      if(adminArray.length == 0){
+        adminArray.push("There are no members currently associated with this lock");
+      }
+      console.log("adminArray in index.js finally is: " + adminArray);
+      callback(adminArray);
     }
-    callback(adminArray);
+  });
+
   })
 }
 
@@ -770,13 +787,16 @@ exports.getLockAdmins = function(lockId, callback) {
  * @param: username, userToAdmin, lockId
  * @return: message based on if it was successful
  */
-exports.addAdmins = function(username, userToAdmin, lockId, callback) {
+ exports.addAdmins = function(username, userToAdmin, lockId, callback) {
   getUser(username, function(user) {
     //check if this user can add admins
     getUser(username, function(user) {
       if(isOwner(user, lockId)) {
         //finds the user that wants to be changed to admin
         db.collection("users").find({"username": userToAdmin}).toArray((err, result) => {
+          if(result[0] == undefined){
+            return;
+          }
           //find correct lock inside the locks array
           for(let i = 0; i < result[0].locks.length; i++){
             //once the lock is found, perform actions on this lock
@@ -791,7 +811,7 @@ exports.addAdmins = function(username, userToAdmin, lockId, callback) {
               //set the current index to the new lock object
               result[0].locks[i] = lockObject;
               //find the user again and update it, setting the new locks array to the result[0] which is now updated
-              db.collection("users").update({username: userToAdmin}, {$set: {locks: result[0]}}, (err, numberAffected, rawResponse) => {})
+              db.collection("users").update({username: userToAdmin}, {$set: {locks: result[0].locks}}, (err, numberAffected, rawResponse) => {})
               //callback message currently doesn't work
               callback({message:"User is now Admin"});
               return;
@@ -802,7 +822,7 @@ exports.addAdmins = function(username, userToAdmin, lockId, callback) {
     })
   })
 }
-        
+
 /**
  * Unlock the lock and record any changes in the history
  * @param: username, lockId, callback
@@ -945,9 +965,9 @@ exports.authenticate = function(username, fullname,  callback) {
  * @param: username, lockId
  * @return: true if can add, else false
  */
-function canAddAdmin(username,lockId){
+ function canAddAdmin(username,lockId){
    return(isOwner(username, lockId) || isAdmin(username, lockId));
-}
+ }
 
 /**
  * Checks if person can remove an admin, if they can then remove admin by
@@ -956,24 +976,37 @@ function canAddAdmin(username,lockId){
  * @return none
  */
  exports.revokeAdmin = function(username, lockId, otherUser, callback) {
-  // if (canRevokeAdmin(username, lockID, otherUser)) {
-  //   db.collection("users").find({"username": otherUser, "locks.lockId": lockId}).toArray((err, result) => {
-  //     // returns false here if person isn't in lock
-  //     if(result[0] == undefined) {
-  //       callbakc(false);
-  //     }
-
-  //     let role = searchLocks(lockId, result[0].locks);
-  //     if(role == 1) {
-  //       let newRole = 2;
-        
-  //       db.collection("users").update({"username": otherUser, "locks.lockId": lockId}, {$set:{locks.}})
-  //     }
-  //   })
-  // }
-  // else{
-  //   callbakc(false);
-  // }
+  getUser(username, function(user1) {
+    //check if this user can add admins
+    //getUser(otherUser, function(user2) {
+      if(isOwner(user1, lockId)) {
+    //find the user of whom we are going to change the role
+    db.collection("users").find({username: otherUser}).toArray((err, result) => {
+      if(result[0] == undefined){
+        return;
+      }
+      //loop through the lock to find the specific lock Id
+      for(let i = 0; i < result[0].locks.length; i++) {
+        //Once the lock Id is found, create a new lock object
+        if(result[0].locks[i].lockId == lockId) {
+          let lockObject = {
+            "lockId": lockId,
+            "role": 2, 
+            "lockRestrictions": [[-1, -1]], 
+            "unlockRestrictions": [[-1, -1]]
+          };
+        //set the current index to the new lock object
+        result[0].locks[i] = lockObject;
+        //update the database
+        db.collection("users").update({username: otherUser}, {$set:{locks: result[0].locks}}, (err, numberAffected, rawResponse) =>{})
+        //callback message currently doesn't work
+        callback({message: "User is now a member!!!!"});
+        return;
+      }
+    }
+  })
+  }
+})
 }
 
 module.exports.convertToMilitary = convertToMilitary;
