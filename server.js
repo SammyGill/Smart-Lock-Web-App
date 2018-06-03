7e6cfbb3bba1238362df216b8c977aa4ff86ae78
Sammy Gill <sammygill98@gmail.com>
2018-06-02 17:39:35 -0700

    added metalTouch code

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 // lock with ID = 1s
 const io = require('socket.io-client');
 "use strict";
-const socket = io.connect('http://100.91.160.60:3000', {reconnect: true,
-              query: "lockId=5"});
+const socket = io.connect('http://100.81.33.111:3000', {reconnect: true,
+              query: "lockId=20"});
 const Gpio = require("pigpio").Gpio;
 const motor = new Gpio(22, {mode:Gpio.OUTPUT});
 motor.enableInterrupt(Gpio.EITHER_EDGE);
 
 var pulseWidth = 1000;
 var increment = 100;
-
+console.log("on");
 // Add a connect listener
 socket.on('connect', function (data) {
     console.log('Connected!');
 });
 
 socket.on("disconnect", function(data) {
-	motor.servoWrite(1800);
+	motor.servoWrite(1000);
 })
 
 socket.on("defaultState", function(state) {
 	if(state == "locked") {
-		motor.servoWrite(1800);
+		motor.servoWrite(1200);
 	}
 	else {
-		motor.servoWrite(1000);
+		motor.servoWrite(1400);
 	}
 })
 
